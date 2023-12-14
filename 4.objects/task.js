58fa22469a35c5f8a7a64fbe4cc76e0fe223f54f
AltXanderKras <116827812+AltXanderKras@users.noreply.github.com>
2023-12-14 20:20:34 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMark = function(mark) {
+	if (!this.excluded) {
+		this.marks.push(mark);
+	}
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.addMarks = function(...marks) {
+	if (!this.excluded) {
+		this.marks.push(...marks);
+	}
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) return 0;
+	return this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;
+};
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+};
